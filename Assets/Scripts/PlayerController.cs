ce49dd5403c944933a03c5765c508d3b13f949f4
andy myers <myersans@iu.edu>
2022-02-22 14:06:53 -0500

    added OnTriggerEnter function to player controller script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -33,4 +33,13 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+        
+    }
+
 }
