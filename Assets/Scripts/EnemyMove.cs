78c979227ca239bc57ede55046ab7d1bdfafdeca
andy myers <myersans@iu.edu>
2022-02-24 21:25:25 -0500

    Updated EnemyMove script to function properly

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -1,13 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using System.Collections;
+ 
 public class EnemyMove : MonoBehaviour
 {
-    public float moveSpeed = 10f;
-
-    void Update()
+    public Vector3 pointB;
+   
+    IEnumerator Start()
     {
-       transform.Translate(new Vector3(0, 1, 0) * moveSpeed * Time.deltaTime); 
+        var pointA = transform.position;
+        while(true)
+        {
+            yield return StartCoroutine(MoveObject(transform, pointA, pointB, 3.0f));
+            yield return StartCoroutine(MoveObject(transform, pointB, pointA, 3.0f));
+        }
+    }
+   
+    IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+    {
+        var i= 0.0f;
+        var rate= 1.0f/time;
+        while(i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+            yield return null;
+        }
     }
 }
